06d0a8c35679de6e293d3f782ba46e8403396a0b
walafc0 <devel@walafc0.org>
2015-04-29 12:54:30 +0200

    Add second report. This is a draft.

---
+++ sesi_report/include/code/example.c
@@ -0,0 +1,12 @@
+int main(int argc, char** argv)
+{
+  int pid = fork();
+  if ( pid ) 
+    /* I'm the father, do something.. */
+  else
+    /* I'm the son */
+    exec("/path/to/pogram", args[]);
+
+  /* This part is never reach by the son */
+  return EXIT_SUCCESS;
+}  
