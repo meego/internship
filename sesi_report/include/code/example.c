7fe5ed781d8bbd8adf8301b61e7fe3db538a80a9
walafc0 <devel@walafc0.org>
2015-04-29 19:50:44 +0200

    Update comment

--- sesi_report/include/code/example.c
+++ sesi_report/include/code/example.c
@@ -7,6 +7,8 @@ int main(int argc, char** argv)
     /* I'm the son */
     exec("/path/to/pogram", args[]);
 
-  /* This part is never reached by the son */
+  /*  This part is never reached by the son.
+   *  Its code segment is erased by the exec() call
+   */
   return EXIT_SUCCESS;
 }  
